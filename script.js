83677dcc375c3ded714edfbc631bfc443893076a
“mohieddinne” <wissem.aouichi@gmail.com>
2021-02-08 21:08:42 +0100

    weather card

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
 var city="abc";
-
-
 function myFunction() {
     var city = document.getElementById("name").value;
-
-
   console.log(city);
 
   $.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=metric&appid=6df81b18f188eb61c7761f191f5f010e",
   function(data){
   console.log(data);
-  
   var icon = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
   var weather = data.main.temp
   var description= data.weather[0].description;
-  
   $(".icon").attr("src", icon);
   $(".weather").text("The temperture is: "+weather);
   $(".temp").text(description);
   });
-  
-}
-  
+} 
+
 myFunction();
